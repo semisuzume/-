764f5e92aa5ad80bb4243a8bd71529015cb9fb31
semisuzume <kawasemi.sb@gmail.com>
2025-01-19 14:49:19 +0900

    DebugModeを作り始める

---
+++ Assets/Script/DebugManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+    GameObject Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 自分の
+    /// </summary>
+    /// <param name="keys"></param>
+    public void DebugUIUpdate(List<string> keys)
+    {
+        Vector2Int MaxLength = new Vector2Int(0, 0);
+        Dictionary<int, string> TerminationList = new Dictionary<int, string>();
+        foreach (string key in keys)
+        {
+            
+        }
+        {
+
+        }
+    }
+
+    private void BoardUpdate()
+    {
+        
+    }
+}
